dcaae43c1e766b1cb50940ff0642541fd4dc76b0
Jacob Asper <jacobasper191@gmail.com>
2023-05-06 22:35:55 -0400

    add doc comments for each piece of the schema

--- src/schema.rs
+++ src/schema.rs
@@ -18,32 +18,36 @@ impl Query {
 	}
 }
 
-/// A top level
+/// A top level field for all questions
 #[derive(SimpleObject)]
 struct Questions<'a> {
-	question_category: QuestionCategory<'a>,
+	category: QuestionCategory<'a>,
 }
 
 // this must be a struct since GraphQL enums may only contain unit variants
+/// The main categories of interview questions—behavioral being more broad and technical more targeted
 #[derive(SimpleObject)]
 struct QuestionCategory<'a> {
 	behavioral: Vec<BehavioralQuestion<'a>>,
 	technical: Vec<TechnicalQuestion<'a>>,
 }
 
+/// Broad questions regarding reactions to a situation
 #[derive(SimpleObject)]
 struct BehavioralQuestion<'a> {
 	html_content: &'a str,
 }
 
+/// Targeted questions to assess knowledge in a domain
 #[derive(SimpleObject)]
 struct TechnicalQuestion<'a> {
 	html_content: &'a str,
-	subcategory: TechnicalQuestionSubtype,
+	subcategory: TechnicalQuestionSubcategory,
 }
 
+/// Domains of knowledge common in front end interviews
 #[derive(Enum, Copy, Clone, Eq, PartialEq)]
-enum TechnicalQuestionSubtype {
+enum TechnicalQuestionSubcategory {
 	HTML,
 	CSS,
 	JavaScript,
