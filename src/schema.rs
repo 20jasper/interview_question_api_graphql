afb749e7a4fbe52dd1c6fdc44c68d374e0aa4740
Jacob Asper <jacobasper191@gmail.com>
2023-05-06 23:27:26 -0400

    change string slices to String

--- src/schema.rs
+++ src/schema.rs
@@ -8,14 +8,10 @@ pub struct Query;
 #[Object]
 impl Query {
 	async fn questions(&self) -> Questions {
-		let questions_json = get_questions_json();
-
-		let questions: Questions = match serde_json::from_str(&get_questions_json()) {
+		match serde_json::from_str(&get_questions_json()) {
 			Ok(json) => json,
 			Err(error) => panic!("Failed to parse json to type Questions: {error}"),
-		};
-
-		questions
+		}
 	}
 }
 
@@ -30,31 +26,29 @@ fn get_questions_json() -> String {
 
 /// A top level field for all questions
 #[derive(SimpleObject, Deserialize, Serialize)]
-struct Questions<'a> {
+struct Questions {
 	// needed since &str is not owned, and serde cannot deserialize borrowed data
-	#[serde(borrow)]
-	category: QuestionCategory<'a>,
+	category: QuestionCategory,
 }
 
 // this must be a struct since GraphQL enums may only contain unit variants
 /// The main categories of interview questions—behavioral being more broad and technical more targeted
 #[derive(SimpleObject, Serialize, Deserialize)]
-struct QuestionCategory<'a> {
-	#[serde(borrow)]
-	behavioral: Vec<BehavioralQuestion<'a>>,
-	technical: Vec<TechnicalQuestion<'a>>,
+struct QuestionCategory {
+	behavioral: Vec<BehavioralQuestion>,
+	technical: Vec<TechnicalQuestion>,
 }
 
 /// Broad questions regarding reactions to a situation
 #[derive(SimpleObject, Serialize, Deserialize)]
-struct BehavioralQuestion<'a> {
-	html_content: &'a str,
+struct BehavioralQuestion {
+	html_content: String,
 }
 
 /// Targeted questions to assess knowledge in a domain
 #[derive(SimpleObject, Serialize, Deserialize)]
-struct TechnicalQuestion<'a> {
-	html_content: &'a str,
+struct TechnicalQuestion {
+	html_content: String,
 	subcategory: TechnicalQuestionSubcategory,
 }
 
